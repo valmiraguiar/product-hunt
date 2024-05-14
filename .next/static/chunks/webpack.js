e912bf35239de2a4e075925b1a39c9fbcd0d8574
Valmir Aguiar <valmiir.aguiar7@gmail.com>
2024-05-14 09:47:48 -0300

    fix: next install fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e19725d028c4d81f"; }
+/******/ 		__webpack_require__.h = function() { return "03399c6518bf8ef6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
