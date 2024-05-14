9d60197725e5793993ada1f3f5907a65df85cc4d
Valmir Aguiar <valmiir.aguiar7@gmail.com>
2024-05-14 09:50:44 -0300

    fix: upvote component div fix

--- src/presentation/components/layout/internal/VoteButton/index.tsx
+++ src/presentation/components/layout/internal/VoteButton/index.tsx
@@ -4,13 +4,11 @@ import { VoteButtonProps } from './types';
 
 const VoteButton: React.FC<VoteButtonProps> = ({ numberOfVotes }) => {
   return (
-    <div className="h-screen w-screen bg-white p-2 m-64">
-      <div className="flex flex-col items-center justify-center size-[48px] border border-solid border-[#D9E1EC] rounded gap-[2px] pt-2 pb-1">
-        <ArrowUpIcon />
-        <p className="text-[12px] font-semibold text-[#4D5A7E] ">
-          {numberOfVotes}
-        </p>
-      </div>
+    <div className="flex flex-col items-center justify-center size-[48px] border border-solid border-[#D9E1EC] rounded gap-[2px] pt-2 pb-1">
+      <ArrowUpIcon />
+      <p className="text-[12px] font-semibold text-[#4D5A7E] ">
+        {numberOfVotes}
+      </p>
     </div>
   );
 };
--- src/presentation/flows/pages/home/layout.tsx
+++ src/presentation/flows/pages/home/layout.tsx
@@ -1,7 +1,11 @@
 import VoteButton from '@/presentation/components/layout/internal/VoteButton';
 
 const HomeLayout: React.FC = () => {
-  return <VoteButton numberOfVotes="252" />;
+  return (
+    <div className="h-screen w-screen bg-white p-2 m-64">
+      <VoteButton numberOfVotes="252" />
+    </div>
+  );
 };
 
 export default HomeLayout;
